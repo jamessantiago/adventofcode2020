d9c707264404e5187eac67f14cff3d8c3dd89354
jamessantiago <james.r.santiago@gmail.com>
2020-12-13 12:56:44 -0600

    day 12/13

---
+++ csharp/AdventOfCode/Day_12.cs
@@ -0,0 +1,162 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Day_12 : BaseDay
+    {
+        List<(char, long)> commands = new List<(char, long)>();
+
+        public Day_12()
+        {
+            commands = File.ReadLines(InputFilePath).Select(d => (d[0], long.Parse(d.Substring(1)))).ToList();
+        }
+
+        public (long, long) Move(long x, long y, char direction, long dist)
+        {
+            switch (direction)
+            {
+                case 'N':
+                    y -= dist;
+                    break;
+                case 'S':
+                    y += dist;
+                    break;
+                case 'E':
+                    x += dist;
+                    break;
+                case 'W':
+                    x -= dist;         
+                    break;
+                default:
+                    break;
+            }
+            return (x, y);
+        }
+
+        public char GetDirection(long heading)
+        {
+            if (heading == 0) return 'E';
+            if (heading == 90) return 'S';
+            if (heading == 180) return 'W';
+            if (heading == 270) return 'N';
+            throw new Exception();
+        }
+
+        public override string Solve_1()
+        {
+            long heading = 0;
+            long x = 0, y = 0;
+
+            foreach (var command in commands)
+            {
+               if (command.Item1 == 'R')
+                {
+                    heading = (heading + command.Item2) % 360;
+                } else if (command.Item1 == 'L')
+                {
+                    heading += 360;
+                    heading = (heading - command.Item2) % 360;
+                } else if (command.Item1 == 'F')
+                {
+                    (x, y) = Move(x, y, GetDirection(heading), command.Item2);
+                } else
+                {
+                    (x, y) = Move(x, y,command.Item1, command.Item2);
+                }
+            }
+
+            return (Math.Abs(x) + Math.Abs(y)).ToString();
+
+        }
+
+        public long GetDistance(long x1, long y1, long x2, long y2)
+        {
+            return (Math.Abs(x1 - x2) + Math.Abs(y1 - y2));
+        }
+
+        public double GetDistanceExact(long x1, long y1, long x2, long y2)
+        {
+            return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+        }
+
+        private double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        public (long, long) MoveWaypolongAround(long x, long y, double radius, double heading)
+        {
+            long x2 = (long)(x + radius * Math.Cos(DegreeToRadian(heading)));
+            long y2 = (long)(y + radius * Math.Sin(DegreeToRadian(heading)));
+            return (x2, y2);
+        }
+
+        public char GetWaypolongDirection(long shipx, long shipy, long wpx, long wpy)
+        {
+            if (shipx < wpx) return 'E';
+            if (shipx > wpx) return 'W';
+            if (shipy > wpy) return 'S';
+            if (shipy < wpy) return 'N';
+            throw new Exception();
+        }
+
+        public (long, long) GetOffsets(long shipx, long shipy, long wpx, long wpy)
+        {
+            return ((wpx - shipx), (wpy - shipy));
+        }
+
+        public double GetHeading(long shipx, long shipy, long wpx, long wpy)
+        {
+            long xdiff = wpx - shipx;
+            long ydiff = wpy - shipy;
+            return (Math.Atan2(ydiff, xdiff) * 180.0 / Math.PI);
+            //return (360 + heading) % 360;
+        }
+
+        public override string Solve_2()
+        {
+            long shipx = 0, shipy = 0;
+            long wpx = 10, wpy = -1;
+
+            foreach (var command in commands)
+            {
+                if (command.Item1 == 'R')
+                {
+                    double r = GetDistanceExact(shipx, shipy, wpx, wpy);
+                    double curheading = GetHeading(shipx, shipy, wpx, wpy);
+                    double newheading = (curheading + command.Item2) % 360; 
+                    (wpx, wpy) = MoveWaypolongAround(shipx, shipy, r, newheading);
+                }
+                else if (command.Item1 == 'L')
+                {
+                    double r = GetDistanceExact(shipx, shipy, wpx, wpy);
+                    double curheading = GetHeading(shipx, shipy, wpx, wpy);
+                    double newheading = ((curheading + 360) - command.Item2) % 360;
+                    (wpx, wpy) = MoveWaypolongAround(shipx, shipy, r, newheading);
+                }
+                else if (command.Item1 == 'F')
+                {
+                    long offsetx = 0, offsety = 0;
+                    (offsetx, offsety) = GetOffsets(shipx, shipy, wpx, wpy);
+                    long distance = GetDistance(shipx, shipy, wpx, wpy);
+                    shipx += offsetx * command.Item2;
+                    shipy += offsety * command.Item2;
+                    wpx = offsetx + shipx; wpy = offsety + shipy;
+                }
+                else
+                {
+                    (wpx, wpy) = Move(wpx, wpy, command.Item1, command.Item2);
+                }
+            }
+
+            return (Math.Abs(shipx) + Math.Abs(shipy)).ToString();
+        }
+    }
+}
---
+++ csharp/AdventOfCode/Inputs/Day_13.cs
@@ -0,0 +1,190 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Z3;
+
+namespace AdventOfCode.Inputs
+{
+    public static class BetterEnumerable
+    {
+        public static IEnumerable<int> SteppedRange(int fromInclusive, int toExclusive, int step)
+        {
+            for (var i = fromInclusive; i < toExclusive; i += step)
+            {
+                yield return i;
+            }
+        }
+    }
+
+    public class Day_13 : BaseDay
+    {
+        int departure = 0;
+        List<int> buses;
+        int[] buses2;
+
+        public Day_13()
+        {
+            var lines = File.ReadAllLines(InputFilePath);
+            departure = int.Parse(lines[0]);
+            buses = lines[1].Split(',').Select(d => d != "x" ? int.Parse(d) : 0).Where(d => d != 0).ToList();            
+            buses.Sort();
+
+            buses2 = lines[1].Split(',').Select(d => d != "x" ? int.Parse(d) : 0).ToArray();
+        }
+
+        public override string Solve_1()
+        {
+            int departbus = 0;
+            int waittime = Int32.MaxValue;
+            Dictionary<int, int> selectedbuses = new Dictionary<int, int>();
+            foreach (var bus in buses)
+            {
+                int time = 0;
+                while (time < departure)
+                {
+                    time += bus;
+                }
+                selectedbuses.Add(bus, time - departure);
+
+            }
+
+            foreach (var bus in selectedbuses)
+            {
+                if (bus.Value < waittime)
+                {
+                    waittime = bus.Value;
+                    departbus = bus.Key;
+                }
+            }
+
+            return (waittime * departbus).ToString();
+        }
+
+        
+
+        public override string Solve_2()
+        {
+            //Context ctx = new Context(new Dictionary<string, string>() { { "model", "true" } });
+            //List<IntExpr> X = new List<IntExpr>();
+            //List<BoolExpr> val_c = new List<BoolExpr>();
+            //List<BoolExpr> div_rules_c = new List<BoolExpr>();
+            //List<BoolExpr> cons_rules_c = new List<BoolExpr>();
+            //int indx = 0;
+            //for (int i = 0; i < buses2.Length; i++)
+            //{
+            //    if (buses2[i] == 0) continue;
+                
+            //    X.Add((IntExpr)ctx.MkConst(ctx.MkSymbol("x_" + (indx + 1)), ctx.IntSort));
+            //    val_c.Add(ctx.MkLe(ctx.MkInt(100000000000000), X[indx]));
+            //    //val_c.Add(ctx.MkLe(ctx.MkInt(0), X[indx]));
+            //    div_rules_c.Add(ctx.MkEq(ctx.MkMod(X[indx], ctx.MkInt(buses2[i])), ctx.MkInt(0)));
+            //    if (i != 0)
+            //        cons_rules_c.Add(ctx.MkEq(X[indx], ctx.MkAdd(X[0], ctx.MkInt(i))));
+            //    indx++;
+            //}
+
+            //BoolExpr buses_c = ctx.MkTrue();
+            //buses_c = ctx.MkAnd(ctx.MkAnd(val_c), buses_c);
+            //buses_c = ctx.MkAnd(ctx.MkAnd(div_rules_c), buses_c);
+            //buses_c = ctx.MkAnd(ctx.MkAnd(cons_rules_c), buses_c);
+
+            //Microsoft.Z3.Solver s = ctx.MkSolver();
+            //s.Assert(buses_c);
+
+            //if (s.Check() == Status.SATISFIABLE)
+            //{
+            //    Model m = s.Model;
+            //    var first = m.Evaluate(X[0]);
+            //    return first.ToString();
+            //} else
+            //{
+            //    return "no";
+            //}
+
+            long foundval = 0;
+            //Dictionary<int, int> xmemo = new Dictionary<int, int>();
+            //Dictionary<int, long> buscount = new Dictionary<int, long>();
+            int[] xmemo = new int[buses2.Length];
+            long[] buscount = new long[buses2.Length];
+            int busleng = buses2.Length;
+            List<int> nonxbuses = new List<int>();
+
+            for (int i = 0; i < busleng; i++)
+            {
+                if (buses2[i] == 0) continue;
+                int numxes = 0;
+                for (int j = i + 1; j < busleng && buses2[j] == 0; j++) if (buses2[j] == 0) numxes++;
+                xmemo[i] = numxes;
+                nonxbuses.Add(i);
+            }
+
+            //for (long i = 100000000000000; i < Int64.MaxValue; i += buses2[0])
+            //{
+            //    long last = i;
+            //    bool found = true;
+            //    for (int b = 1; b < busleng; b++)
+            //    {
+            //        if (buses2[b] == 0) continue;
+            //        while (buscount[b] < last) buscount[b] += buses2[b];
+            //        if ((i + b + xmemo[b]) != buscount[b])
+            //        {
+            //            found = false;
+            //            break;
+            //        }
+            //    }
+            //    if (found)
+            //    {
+            //        foundval = i;
+            //        break;
+            //    }
+            //}
+
+
+
+            //for (long i = 100000000000004; i < Int64.MaxValue; i += buses2[0])
+            //{
+            //    int numxes = xmemo[0];
+            //    if ((i + numxes + 1) % buses2[1 + numxes] != 0) continue;
+
+            //    bool found = false;
+            //    for (int b = 1; b < nonxbuses.Count - 1; b++)
+            //    {
+            //        numxes = xmemo[nonxbuses[b]];
+            //        if ((i + nonxbuses[b] + numxes + 1) % buses2[nonxbuses[b] + numxes + 1] == 0) found = true;
+            //        else found = false;
+
+            //        if (!found) break;
+            //    }
+            //    if (found)
+            //    {
+            //        foundval = i;
+            //        break;
+            //    }
+            //}
+
+            long bus0 = (long)buses2[0];
+            for (int b = 1; b < buses2.Length; b++)
+            {
+                if (buses2[b] == 0) continue;
+                var tmp = buses2[b];
+                while(true)
+                {
+                    foundval += bus0;
+                    if ((foundval + b) % tmp == 0)
+                    {
+                        bus0 *= tmp;
+                        break;
+                    }
+                }
+            }
+
+
+            return foundval.ToString();
+        }
+    }
+}
