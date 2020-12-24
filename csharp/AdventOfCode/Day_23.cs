412834afeb66ebd49db291da9bbdd3863ea7d653
jamessantiago <james.r.santiago@gmail.com>
2020-12-24 00:05:42 -0600

    day 24

---
+++ csharp/AdventOfCode/Day_23.cs
@@ -0,0 +1,182 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Z3;
+using System.Collections.Immutable;
+
+namespace AdventOfCode
+{
+    public class Day_23 : BaseDay
+    {
+        ImmutableList<int> cups;
+        ImmutableList<int> origcups;
+
+        public Day_23()
+        {
+            cups = File.ReadAllText(InputFilePath).ToCharArray().Select(d => int.Parse(d.ToString())).ToImmutableList();
+            origcups = cups.ToImmutableList();
+        }
+
+        public override string Solve_1()
+        {
+
+            int cuplen = cups.Count();
+            int curcup = cups.First();
+
+            for (int i = 0; i < 100; i++)
+            {
+                //Console.WriteLine("Cups: {0}", String.Join(", ", cups));
+                var cumpindx = cups.IndexOf(curcup);
+                var cup1 = cups[(cumpindx + 1) % cuplen];
+                var cup2 = cups[(cumpindx + 2) % cuplen];
+                var cup3 = cups[(cumpindx + 3) % cuplen];
+                cups = cups.Remove(cup1);
+                cups = cups.Remove(cup2);
+                cups = cups.Remove(cup3);
+                //Console.WriteLine("Take: {0} {1} {2}", cup1, cup2, cup3);
+                cumpindx = cups.IndexOf(curcup);
+                curcup = cups[(cumpindx + 1) % (cuplen - 3)];
+
+                var des = cups[cumpindx] - 1;
+                var mincup = cups.Min();
+                var maxcup = cups.Max();
+                if (des < mincup) des = maxcup;
+                while (des == cup1 || des == cup2 || des == cup3)
+                {
+                    des--;
+                    if (des < mincup) des = maxcup;
+                }
+                //Console.WriteLine("Dest: {0}\n", des);
+                var newindx = cups.IndexOf(des) + 1;
+                if (newindx == cuplen - 3)
+                {
+                    cups = cups.Add(cup1);
+                    cups = cups.Add(cup2);
+                    cups = cups.Add(cup3);
+                }
+                else
+                {
+                    cups = cups.Insert(newindx, cup3);
+                    cups = cups.Insert(newindx, cup2);
+                    cups = cups.Insert(newindx, cup1);
+                }
+            }
+
+            var result = "";
+            for (int i = cups.IndexOf(1) + 1; i < cups.IndexOf(1) + cups.Count; i++)
+            {
+                result += cups[i % cups.Count].ToString();
+            }
+
+            return result;
+        }
+
+        public override string Solve_2()
+        {
+            //cups = origcups;
+            //int mc = cups.Max();
+            //int cc = cups.Count;
+            //for (int j = 0; j < 1000000 - cc; j++) cups = cups.Add(j + mc + 1);
+            //int cuplen = cups.Count();
+            //int offset = 0;
+
+            //int mincup = cups.Min();
+            //int maxcup = cups.Max();
+
+            //int i = 0;
+
+            //for (i = 0; i < 10000000; i++)
+            //{
+            //    //Console.WriteLine("Cups: {0}", String.Join(", ", cups));
+            //    var cup1 = cups[(i + offset + 1) % cuplen];
+            //    var cup2 = cups[(i + offset + 2) % cuplen];
+            //    var cup3 = cups[(i + offset + 3) % cuplen];
+
+            //    var des = cups[(i + offset) % cuplen] - 1;
+
+            //    if (des < mincup) des = maxcup;
+            //    while (des == cup1 || des == cup2 || des == cup3)
+            //    {
+            //        des--;
+            //        if (des < mincup) des = maxcup;
+            //    }
+
+            //    cups = cups.RemoveAt((i + offset + 1) % cuplen);
+            //    cups = cups.RemoveAt((i + offset + 2) % cuplen);
+            //    cups = cups.RemoveAt((i + offset + 3) % cuplen);
+            //    //Console.WriteLine("Take: {0} {1} {2}", cup1, cup2, cup3);
+
+            //    //Console.WriteLine("Dest: {0}\n", des);           
+            //    var newindx = cups.IndexOf(des) + 1;
+
+            //    if (newindx < (i + offset) % cuplen) offset++;
+            //    if (newindx < (i + offset + 1) % cuplen) offset++;
+            //    if (newindx < (i + offset + 2) % cuplen) offset++;
+            //    if (newindx == cuplen - 3)
+            //    {
+            //        cups = cups.Add(cup1);
+            //        cups = cups.Add(cup2);
+            //        cups = cups.Add(cup3);
+            //    }
+            //    else
+            //    {
+            //        cups = cups.Insert(newindx, cup3);
+            //        cups = cups.Insert(newindx, cup2);
+            //        cups = cups.Insert(newindx, cup1);
+            //    }
+            //}
+
+
+
+            //var ca = cups[cups.IndexOf(1) + 1];
+            //var cb = cups[cups.IndexOf(1) + 2];
+
+            cups = origcups;
+            cups = cups.Add(10);
+
+            int[] numbers = new int[1000001];
+            for (int i = 0; i < cups.Count - 1; i++) numbers[cups[i]] = cups[i + 1];
+
+            numbers[0] = 0;
+            numbers[1000000] = cups.First();
+            for (int i = 10; i < 1000000; ++i)
+            {
+                numbers[i] = i + 1;
+            }
+
+            int current = cups.First();
+
+            for (int i = 0; i < 10000000; ++i)
+            {
+                int value = current;
+                int next1 = numbers[current];
+                int next2 = numbers[next1];
+                int next3 = numbers[next2];
+
+                do
+                {
+                    value--;
+                    if (value == 0)
+                        value = 1000000;
+                }
+                while (next1 == value || next2 == value || next3 == value);
+
+                numbers[current] = numbers[next3];
+                numbers[next3] = numbers[value];
+                numbers[value] = next1;
+
+                current = numbers[current];
+            }
+
+            long prod = numbers[1];
+            prod *= numbers[numbers[1]];
+            return prod.ToString();
+        }
+    }
+}
---
+++ csharp/AdventOfCode/Day_24.cs
@@ -0,0 +1,157 @@
+﻿using AoCHelper;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Z3;
+using System.Collections.Immutable;
+
+namespace AdventOfCode
+{
+    public class Day_24 : BaseDay
+    {
+        List<List<string>> rules = new List<List<string>>();
+
+        public Day_24()
+        {            
+            var lines = File.ReadAllLines(InputFilePath);
+            foreach (var line in lines)
+            {
+                var matches = Regex.Matches(line, @"e|se|sw|w|nw|ne");
+                var rule = new List<string>();
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    rule.Add(matches[i].Value);
+                }
+                rules.Add(rule);
+            }
+        }
+
+        public (int,int,int) MoveTile((int, int, int) tile, string direction)
+        {
+            switch (direction)
+            {
+                case "e":
+                    tile.Item3++;
+                    break;
+                case "se":
+                    tile.Item2 += tile.Item1;
+                    tile.Item3 += tile.Item1;
+                    tile.Item1 = 1 - tile.Item1;
+                    break;
+                case "sw":
+                    tile.Item2 += tile.Item1;
+                    tile.Item3 -= (1 - tile.Item1);
+                    tile.Item1 = 1 - tile.Item1;
+                    break;
+                case "w":
+                    tile.Item3--;
+                    break;
+                case "nw":
+                    tile.Item2 -= (1 - tile.Item1);
+                    tile.Item3 -= (1 - tile.Item1);
+                    tile.Item1 = 1 - tile.Item1;
+                    break;
+                case "ne":
+                    tile.Item2 -= (1 - tile.Item1);
+                    tile.Item3 += tile.Item1;
+                    tile.Item1 = 1 - tile.Item1;
+                    break;
+                default:
+                    break;
+            }
+            return tile;
+        }
+
+        public override string Solve_1()
+        {
+            Dictionary<(int, int, int), bool> tiles = new Dictionary<(int, int, int), bool>();
+            foreach (var rule in rules)
+            {
+                (int, int, int) tile = (0, 0, 0);
+                foreach (var direction in rule)
+                {
+                    tile = MoveTile(tile, direction);
+                }
+
+                if (tiles.ContainsKey(tile))
+                    tiles[tile] = !tiles[tile];
+                else
+                    tiles.Add(tile, true);
+            }
+
+            return tiles.Count(d => d.Value).ToString();
+        }
+
+        List<string> directions = new List<string> { "e", "se", "sw", "w", "nw", "ne" };
+
+        public List<(int, int, int)> GetNeighbors((int, int, int) tile)
+        {
+            List<(int, int, int)> neighbors = new List<(int, int, int)>();
+            foreach (var dir in directions)
+            {
+                neighbors.Add(MoveTile(tile, dir));
+            }
+            return neighbors;
+        }
+
+        public override string Solve_2()
+        {
+            Dictionary<(int, int, int), bool> tiles = new Dictionary<(int, int, int), bool>();
+            foreach (var rule in rules)
+            {
+                (int, int, int) tile = (0, 0, 0);
+                foreach (var direction in rule)
+                {
+                    tile = MoveTile(tile, direction);
+                }
+
+                if (tiles.ContainsKey(tile))
+                    tiles[tile] = !tiles[tile];
+                else
+                    tiles.Add(tile, true);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                var changetowhite = tiles.Where(d => d.Value).Where(d =>
+                {
+                    var neis = GetNeighbors(d.Key);
+                    var btcount = tiles.Count(t => neis.Contains(t.Key) && t.Value);
+                    return btcount == 0 || btcount > 2;
+                }).ToList();
+
+                var whitetiles = tiles.Where(d => d.Value).SelectMany(d => GetNeighbors(d.Key)).ToHashSet()
+                    .Where(d => !tiles.ContainsKey(d) || !tiles[d]);
+                
+                var changetoblack = whitetiles.Where(d =>
+                    {
+                        var neis = GetNeighbors(d);
+                        var btcount = tiles.Count(t => neis.Contains(t.Key) && t.Value);
+                        return btcount == 2;
+                    }).ToList();
+
+                foreach (var ctw in changetowhite)
+                {
+                    tiles[ctw.Key] = false;
+                }
+
+                foreach (var ctb in changetoblack)
+                {
+                    if (tiles.ContainsKey(ctb))
+                        tiles[ctb] = true;
+                    else
+                        tiles.Add(ctb, true);
+                }
+
+                //Console.WriteLine("Day {0}: {1}", i + 1, tiles.Count(d => d.Value));
+            }
+
+            return tiles.Count(d => d.Value).ToString();
+        }
+    }
+}
